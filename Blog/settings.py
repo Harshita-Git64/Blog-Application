d78228599c7d85c417dd50f1f3d1a5fd0995ad28
Harshita-Git64 <harshitapaliwal789@gmail.com>
2024-08-08 01:17:20 +0530

    modifies application

--- Blog/settings.py
+++ Blog/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'ejcw-d5t67dgrz$_evfgd#kz7y_ku70urb5bnncn2^3*&o0l&@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
--- home/forms.py
+++ home/forms.py
@@ -1,8 +1,6 @@
 from django import forms
 from .models import BlogPost
-
-
-        
+   
 class BlogPostForm(forms.ModelForm):
     class Meta:
         model = BlogPost
